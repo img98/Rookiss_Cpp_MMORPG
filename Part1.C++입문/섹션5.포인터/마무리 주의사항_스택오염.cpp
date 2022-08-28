492a82916ad86a8aa8d55b04617aa831f5e042f0
img98 <nichy98@naver.com>
2022-08-28 18:12:27 +0900

    참조, 배열, 다중포인터, 포인터마무리 업데이트

---
+++ Part1.C++입문/섹션5.포인터/마무리 주의사항_스택오염.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int* TestPointer()
+{
+	int a = 1;
+	return &a;
+}
+void TestWrong(int* ptr)
+{
+	int arr[100] = {};
+	arr[99] = 0xAAAAAAAA;
+	*ptr = 0x12341234;
+}
+
+int main()
+{
+	int* pointer = TestPointer(); //스택이라서 사라질 a의 주소에 연결해버림.
+	TestWrong(pointer);
+
+	return 0;
+}
