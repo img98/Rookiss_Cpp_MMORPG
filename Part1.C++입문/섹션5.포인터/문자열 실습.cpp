eeff7a4e69718c6146c979fa814936709e8b5cf8
img98 <nichy98@naver.com>
2022-08-29 17:25:42 +0900

    포인터,문자열 실습, 파일분할(헤더) 업데이트

---
+++ Part1.C++입문/섹션5.포인터/문자열 실습.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+using namespace std;
+
+// 문자열의 길이 함수 만들기 strlen
+int StringLength(char* str)
+{
+	if (str == nullptr)
+		return NULL;
+	int idx = 0;
+	while (str[idx] != '\0')
+		idx++;
+	return idx;
+}
+//문자열복사하기 strcpy_s
+char* StringCopy(char* dest, char* src)
+{
+	int idx = 0;
+	while (src[idx] != '\0')
+	{
+		dest[idx] = src[idx];
+		idx++;
+	}
+	dest[idx] = '\0';
+
+	return dest;
+
+	/*	
+	char* ret = dest;
+
+	 while(*src!='\0')
+	{
+		*dest = *src;
+		dest++;
+		src++
+	}
+	  *dest='\0'
+	  
+	return ret;  
+	*/	
+}
+//문자열 뒤에 이어쓰기 strcat
+char* StringCat(char* dest,char* src)
+{
+	char* ret = dest;
+
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return ret;
+}
+//문자열 비교 strcomp
+bool StringCmp(char* dest, char* src)
+{
+	while (*dest != '\0' || *src != '\0')
+	{
+		if (*dest > *src)
+			return 1;
+		else if (*dest < *src)
+			return -1;
+		dest++;
+		src++;
+	}
+	return 0;
+}
+//문자열 뒤집기
+void ReverseString(char* str)
+{
+	int len = StringLength(str);
+	char tmp;
+	for (int i = 0; i < len / 2; i++)
+	{
+		tmp = *(str + i);
+		*(str + i) = *(str + len - 1 - i);
+		*(str + len - 1 - i) = tmp;
+	}
+}
+
+int main()
+{
+	char a[100] = "hello";
+	char b[100] = "hella";
+	int len = StringLength(a);
+	cout << len<<'\n';
+	
+	//StringCopy(b, a);
+	cout << b << '\n';
+	//StringCat(a, b);
+	cout << a << '\n';
+	//int value = StringCmp(a, b);
+	//cout << value;
+	ReverseString(b);
+	cout << b;
+
+	return 0;
+}
---
+++ Part1.C++입문/섹션5.포인터/소용돌이 실습.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+using namespace std;
+#include <iomanip>
+
+const int MAX = 100;
+int board[MAX][MAX] = {};
+int N;
+
+void PrintBoard()
+{
+	for (int y = 0; y < N; y++)
+	{
+		for (int x = 0; x < N; x++)
+			cout << setfill('0') << setw(2) << board[y][x] << " ";
+		cout << endl;
+	}
+}
+
+enum DIR
+{
+	RIGHT = 0,
+	DOWN = 1,
+	LEFT = 2,
+	UP = 3,
+};
+
+bool CanGo(int y, int x)
+{
+	if (y < 0 || y >= N) //보드판 위를 벗어나거나
+		return false;
+	if (x < 0 || x >= N)
+		return false;
+	if (board[y][x] != 0) //이미 간적이 있다면
+		return false;
+	return true;
+}
+void SetBoard()
+{
+	int dir = RIGHT;
+	int num = 1;
+	int y = 0;
+	int x = 0;
+
+	int dy[] = { 0,1,0,-1 };
+	int dx[] = { 1,0,-1,0 };
+
+	while (true)
+	{
+		board[y][x] = num;
+		
+		if (num == N * N)
+			break;
+
+		int nextY = y + dy[dir];
+		int nextX = x + dx[dir];
+
+		if (CanGo(nextY, nextX))
+		{
+			y = nextY;
+			x = nextX;
+			num++;
+		}
+		else
+		{
+			dir++;
+			dir %= 4;
+		}
+	}
+}
+
+int main()
+{
+	cout << "Input Number : ";
+	cin >> N;
+	SetBoard();
+	PrintBoard();
+	return 0;
+}
