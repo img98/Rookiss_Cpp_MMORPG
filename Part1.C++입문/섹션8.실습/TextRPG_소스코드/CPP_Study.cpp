96da8b52f88a775acab8bcaa6620152f34f0c34e
img98 <nichy98@naver.com>
2022-10-19 09:50:05 +0900

    TxtRPG(OOP) 소스코드 업데이트

---
+++ Part1.C++입문/섹션8.실습/TextRPG_소스코드/CPP_Study.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+#include "Game.h"
+// 오늘의 주제 : TextRPG #4
+
+int main()
+{
+	srand((unsigned int)time(nullptr));
+
+	Game game;
+	game.Init();
+
+	while (true)
+	{
+		game.Update();
+	}
+	
+	return 0;
+}
